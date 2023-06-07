04a67a4633c3b682176420f8a8335d9b3007173b
EllaW34 <ellaweathers3@gmail.com>
2023-06-07 08:54:12 -0700

    Initial Upload

---
+++ Birds.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+	$("#hummingPic1").click(function() {
+		$("#humming1b").toggleClass("humming1");
+	})
+	$("#hummingPic2").click(function() {
+		$("#humming2b").toggleClass("humming2");
+	})
+	$("#hummingPic3").click(function() {
+		$("#humming3b").toggleClass("humming3");
+	})
+	
+	$("#OstrichPic1").click(function() {
+		$("#Ostrich1b").toggleClass("Ostrich1");
+	})
+	$("#OstrichPic2").click(function() {
+		$("#Ostrich2b").toggleClass("Ostrich2");
+	})
+	$("#OstrichPic3").click(function() {
+		$("#Ostrich3b").toggleClass("Ostrich3");
+	})
+	
+	$("#PelicanPic1").click(function() {
+		$("#Pelican1b").toggleClass("Pelican1");
+	})
+	$("#PelicanPic2").click(function() {
+		$("#Pelican2b").toggleClass("Pelican2");
+	})
+	$("#PelicanPic3").click(function() {
+		$("#Pelican3b").toggleClass("Pelican3");
+	})
+});
